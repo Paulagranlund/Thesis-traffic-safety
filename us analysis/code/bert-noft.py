0cf46f68b5b31c7220366c88e6e54f5fc62ae3e4
Paulagranlund <paulagranlund@hotmail.com>
2026-02-16 15:40:55 +0100

    Update bert-noft.py

--- us analysis/code/bert-noft.py
+++ us analysis/code/bert-noft.py
@@ -9,8 +9,6 @@ EXCEL_PATH = "US Analysis/data/case_info_2020.xlsx"
 df = pd.read_excel(EXCEL_PATH)  # For example, contains ["SUMMARY", "LABEL"]
 texts = df["SUMMARY"].astype(str).tolist()
 labels = df["MANCOLL"].tolist()  # Labels are 0, 1, 2, 4, 5, 6, 9
-print("hej")
-print(df)
 
 # 2. Load pre-trained BERT (no fine-tuning)
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
