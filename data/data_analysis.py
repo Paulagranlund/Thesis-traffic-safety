8ffb3757f0103255005b3a0b44d1a52e918f32c8
MittaHage <s215005@dtu.dk>
2026-02-18 11:55:54 +0100

    changes

---
+++ data/data_analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+from extract_excel_info.py import extract_text_and_labels
+#
+# EXCEL_PATH = "data/....xlsx"
+#df = pd.read_excel(EXCEL_PATH)  # For example, contains ["SUMMARY", "LABEL"]
+df = extract_text_and_labels("data/2025 only.xlsx", sheet_number=0)
+texts = df["SUMMARY"].astype(str).tolist()
+labels = df["MANCOLL"].tolist()  # Labels are 0, 1, 2, 4, 5, 6, 9
+df.head()
+
+
+#priliminary analysis of label distribution
+label_counts = df['MANCOLL'].value_counts()
+print("Label Distribution:")
+print(label_counts)
+
